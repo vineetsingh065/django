973505dc04054e2b6d1b9824a0de871ed353df30
vineetsingh065@gmail.com <Hunt123@>
2020-07-20 20:32:58 +0530

    models configuration

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,12 +1,20 @@
 from __future__ import unicode_literals
+
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 # Create your models here.
 
 class Main(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=50)
     about = models.TextField()
+    fb = models.CharField(default='-', max_length=50)
+    tw = models.CharField(default='-', max_length=50)
+    yt = models.CharField(default='-', max_length=50)
+    contact = models.CharField(default='0', max_length=12)
 
-    # def __str__(self):
-    #     return self.name
+    site_name = models.CharField(default='-', max_length=50)
+
+    def __str__(self):
+        return self.site_name + " | " + str(self.pk)
